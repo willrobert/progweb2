9bad3256e36f701490618d11bc396e601d08ecb8
Felipe Matheus <fmcm@icomp.ufam.edu.br>
2019-08-29 16:25:19 -0400

    First express commit

---
+++ express/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.get("/", function(req, res) {
+ res.send("Hello world!");
+});
+app.listen(3000, function() {
+ console.log("Express app iniciada na porta 3000.");
+});
