95af608a29d34fcd6feb638022f15945d222810b
Felipe Matheus <fmcm@icomp.ufam.edu.br>
2019-08-29 17:46:45 -0400

    Do some changes

--- express/app.js
+++ express/app.js
@@ -1,7 +1,20 @@
+// var express = require("express");
+// var app = express();
+// var logger = require("morgan");
+
+// app.use(logger("tiny"));
+// app.use(function(req, res) {
+//  res.end("Hello, world!");
+// });
+// app.listen(3000);
+
 const express = require("express");
 const app = express();
-app.get("/", function(req, res) {
- res.send("Hello world!");
+var logger = require("morgan");
+
+app.use(logger("short"));
+app.use(function(req, res) {
+ res.end("Hello, world!");
 });
 app.listen(3000, function() {
  console.log("Express app iniciada na porta 3000.");
